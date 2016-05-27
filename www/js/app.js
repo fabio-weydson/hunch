387298bd3acc3b7e36a3f1022a95edb3e9b24be4
fabio-weydson <fabioweydson@gmail.com>
2016-05-27 14:19:26 -0300

    splash e config

--- www/js/app.js
+++ www/js/app.js
@@ -9,7 +9,6 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services',
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    navigator.splashscreen.hide();
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
